54314dfb4acc12ebe7fa543b384d424ac8a65a75
Pavan Kumar Raju <pavan.mailme@gmail.com>
2013-07-30 18:38:38 +0530

    keyword runner extends from webdriverinitiator
    Keyword Driven Runner now extends from WebDriverInitiator

--- src/main/java/com/olo/keyworddriven/Runner.java
+++ src/main/java/com/olo/keyworddriven/Runner.java
@@ -5,11 +5,12 @@ import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
 import com.olo.annotations.Reporter;
-import com.olo.initiator.BrowserBotInitiator;
+import com.olo.bot.BrowserBot;
+import com.olo.initiator.WebDriverInitiator;
 import com.olo.keyworddriven.Keywords;
 
 
-public class Runner extends BrowserBotInitiator implements ITest{
+public class Runner extends WebDriverInitiator implements ITest{
 	
 	private String testFilePath;
 	
@@ -27,6 +28,7 @@ public class Runner extends BrowserBotInitiator implements ITest{
 	@Reporter(name=com.olo.annotations.KeywordDriven.class)
 	@Test
 	public void keywordTest(ITestContext ctx) throws Exception{
+		BrowserBot browser = new BrowserBot(driver);
 		Execution execution = new Execution(testFilePath, browser, new Keywords(browser));
 		execution.run(ctx);
 	}
