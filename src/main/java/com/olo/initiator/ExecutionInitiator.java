a40fbbf7aa684721ec8776717a4cd654e26bd9d9
Pavan Kumar Raju <pavan.mailme@gmail.com>
2013-07-30 17:00:47 +0530

    rolling back from test to main
    rolling back file from src/test/java to src/main/java

---
+++ src/main/java/com/olo/initiator/ExecutionInitiator.java
@@ -0,0 +1,66 @@
+package com.olo.initiator;
+
+import static com.olo.util.PropertyReader.configProp;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testng.TestNG;
+
+import com.olo.listeners.*;
+
+public class ExecutionInitiator {
+	
+	private static final Logger logger = LogManager.getLogger(ExecutionInitiator.class.getName());
+	
+	public static void main(String[] args){
+		
+		try {
+			
+			if(args.length==0){
+				logger.error("No Suite Files to Run");
+				throw new Exception("No Suite Files specified");
+			}
+			
+			String[] suitsToRun=args[0].split(",");
+			
+			if ( args.length > 1){
+				configProp.setProperty("browser", args[1]);
+			}
+			
+			List<String> suiteFiles = new ArrayList<String>();
+			for(int i=0;i<suitsToRun.length;i++){
+				File suiteFile = new File(suitsToRun[i]);
+				if(suiteFile.exists()){
+					suiteFiles.add(suiteFile.getPath());
+				}
+			}
+			
+			TestNG testng = new TestNG();
+			TestListener testListener = new TestListener();
+			SuiteListener suiteListner = new SuiteListener();
+			Reporter reporter = new Reporter();
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy_HH-mm-ss");
+			testng.setTestSuites(suiteFiles);
+			testng.setUseDefaultListeners(false);
+			testng.addListener(testListener);
+			testng.addListener(suiteListner);
+			testng.addListener(reporter);
+			
+			String testOutputDirectoryAppend="ExecutionReport-"+formatter.format(Calendar.getInstance().getTime());
+			String testOutputDirectory="test-output/"+testOutputDirectoryAppend;
+			testng.setOutputDirectory(testOutputDirectory);
+			testng.run();
+			logger.info("Execution Completed !!!");
+			logger.info("Execution Reports are located in "+testOutputDirectory);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
